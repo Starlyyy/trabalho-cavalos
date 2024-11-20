31abaf91ffbf125b595f112485405852d721e805
Izabella da Costa <130591081+Starlyyy@users.noreply.github.com>
2024-11-20 19:08:29 -0300

    Update Dinheiro.php

--- modelo/Dinheiro.php
+++ modelo/Dinheiro.php
@@ -2,8 +2,17 @@
 
 require_once('Aposta.php');
 
-class Dinheiro extends Aposta{
+class Dinheiro extends Aposta implements IApostar{
     private float $valor;
+
+    public function getAposta()
+    {
+        $dados = "A aposta do tipo 'dinheiro' foi realizada no dia " . $this->getDataAposta();
+        $dados .= " no valor de: " . $this->valor;
+        $dados .= " no cavalo: " . $this->getCavalo()->getNomeCavalo() ."\n";
+
+        return $dados;
+    }
 
     public function getValor(): float
     {
