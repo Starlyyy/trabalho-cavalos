069cf6ba641341fc55479626f6b243c34eaabcdd
Izabella da Costa <130591081+Starlyyy@users.noreply.github.com>
2024-11-20 19:09:11 -0300

    Update Bem.php

--- modelo/Bem.php
+++ modelo/Bem.php
@@ -2,10 +2,19 @@
 
 require_once('Aposta.php');
 
-class Bem extends Aposta{
+class Bem extends Aposta implements IApostar{
     private string $tipoBem;
     private int $qualidade;
 
+    public function getAposta()
+    {
+        $dados = "A aposta do tipo 'bem' foi realizada no dia " . $this->getDataAposta();
+        $dados .= " com o bem: " . $this->tipoBem;
+        $dados .= " com qualidade:  " . $this->qualidade . "/10";
+        $dados .= " no cavalo: ". $this->getCavalo()->getNomeCavalo() . "\n";
+
+        return $dados;
+    }
 
     public function getTipoBem(): string
     {
